d8984eec1d75379a8313c5a83ebf54f3dbf2d6a3
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-09-18 19:53:40 -0300

    rerun projetos

--- microsoft-learn/persist-and-retrieve-relational-data-by-using-efcore/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ microsoft-learn/persist-and-retrieve-relational-data-by-using-efcore/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e415f0e6d399593071b95444f9d686e043645310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88783be5563da462cd8bdcb3604509f5241e166")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
